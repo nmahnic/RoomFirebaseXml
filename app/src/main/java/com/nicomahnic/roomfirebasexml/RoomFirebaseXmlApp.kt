e924759c2a944afa9ecd31123534ba3ef6da04db
Nicolas Mahnic <nico.mahnic.ar@gmail.com>
2023-11-16 13:17:10 +0100

    add feature_users module and add room

--- app/src/main/java/com/nicomahnic/roomfirebasexml/RoomFirebaseXmlApp.kt
+++ app/src/main/java/com/nicomahnic/roomfirebasexml/RoomFirebaseXmlApp.kt
@@ -1,6 +1,8 @@
 package com.nicomahnic.roomfirebasexml
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class RoomFirebaseXmlApp : Application() {
@@ -8,7 +10,11 @@ class RoomFirebaseXmlApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidLogger()
+            androidContext(this@RoomFirebaseXmlApp)
+            modules(listOf(
 
+            ))
         }
     }
 }
